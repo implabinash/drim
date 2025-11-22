f2c6cf89b8e402b592c33843c54dd73a6e5fa0f9
Abinash Sahoo <implabinash@gmail.com>
2025-11-22 19:19:06 +0530

    feat: make the landingpage

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,7 @@
+<footer class=" border-t border-black pt-10">
+	<div class="flex flex-col gap-8 text-sm font-bold sm:flex-row">
+		<a href="https://x.com/implabinash" target="_blank">Twitter</a>
+		<a href="https://github.com/implabinash/drim" target="_blank">GitHub</a>
+		<a href="https://cal.com/implabinash/drim" target="_blank">Let’s Talk</a>
+	</div>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import Footer from "$lib/components/Footer.svelte";
+	import Problem from "$lib/components/Problem.svelte";
 </script>
 
 <svelte:head>
@@ -6,4 +8,21 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>DevRel Impact Model (DIM)</section>
+<section class="mx-auto max-w-2xl px-6 py-20 text-base sm:text-lg">
+	<nav class="mb-20 flex justify-end gap-8 text-sm font-bold">
+		<a href="https://x.com/implabinash" target="_blank">Twitter</a>
+		<a href="https://github.com/implabinash/drim" target="_blank">GitHub</a>
+		<a href="https://cal.com/implabinash/drim" target="_blank">Let’s Talk</a>
+	</nav>
+
+	<header class="mb-20">
+		<h1 class="mb-2 text-3xl font-bold">DRIM</h1>
+		<div class="text-sm font-bold text-gray-500">DevRel Impact Model</div>
+	</header>
+
+	<main class="mb-20">
+		<Problem />
+	</main>
+
+	<Footer />
+</section>
